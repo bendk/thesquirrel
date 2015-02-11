985d11d1a5e5a47b39f427ce3d5eae2783626598
Ben Dean-Kawamura <ben@pculture.org>
2015-02-11 16:36:20 -0500

    Added user login/logout
    Added support for submenus on the nav

--- thesquirrel/urls.py
+++ thesquirrel/urls.py
@@ -23,6 +23,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login',
         name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+        name='logout'),
 )
 
 if settings.DEV:
