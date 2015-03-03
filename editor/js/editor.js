e2873c9a7dcc86ebe9891d2a58487bb325d49b70
Ben Dean-Kawamura <ben@pculture.org>
2015-03-03 15:24:26 -0500

    Split up editor JS

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -4,40 +4,19 @@ $(document).ready(function() {
     $('fieldset.editor').each(function() {
         var fieldset = $(this);
         var textarea = $('textarea', this);
-        var addMedia = $('.add-media', this);
-        var addMediaProgress = $('.add-media-progress', this);
+
+        editorUploadImage(fieldset, textarea);
+        editorPreview(fieldset, textarea);
+    });
+
+    function editorUploadImage(fieldset, textarea) {
+        var addMedia = $('.add-media', fieldset);
+        var addMediaProgress = $('.add-media-progress', fieldset);
         var addMediaProgressBar = $('.bar', addMediaProgress);
-        var addImage = $('button.add-image', this);
-        var preview = $('button.preview', this);
+        var addImage = $('button.add-image', fieldset);
         var uploadFile = $('<input type="file" name="file">');
         var uploadFileForm = $('<form>').append(uploadFile);
         body.append(uploadFileForm);
-
-        preview.click(function() {
-            var overlay = $('<div id="overlay">');
-            $('body').append(overlay);
-            var modal = $('#editor-preview-modal');
-            modal.show();
-            $('button.close', modal).click(function() {
-                modal.hide();
-                overlay.remove();
-            });
-            $.ajax({
-                url: '/editor/preview/',
-                type: 'GET',
-                data: {
-                    'body': textarea.val(),
-                },
-                headers: {
-                    'X-CSRFToken': $.cookie('csrftoken'),
-                },
-                success: function(responseData) {
-                    $('i.fa-spinner', modal).hide();
-                    $('div.body', modal).html(responseData['body']);
-                }
-            });
-            return false;
-        });
 
         addImage.click(function() {
             uploadFile.click();
@@ -67,7 +46,36 @@ $(document).ready(function() {
                 });
             }
         });
-    });
+    }
+
+    function editorPreview(fieldset, textarea) {
+        var preview = $('button.preview', fieldset);
+        preview.click(function() {
+            var overlay = $('<div id="overlay">');
+            $('body').append(overlay);
+            var modal = $('#editor-preview-modal');
+            modal.show();
+            $('button.close', modal).click(function() {
+                modal.hide();
+                overlay.remove();
+            });
+            $.ajax({
+                url: '/editor/preview/',
+                type: 'GET',
+                data: {
+                    'body': textarea.val(),
+                },
+                headers: {
+                    'X-CSRFToken': $.cookie('csrftoken'),
+                },
+                success: function(responseData) {
+                    $('i.fa-spinner', modal).hide();
+                    $('div.body', modal).html(responseData['body']);
+                }
+            });
+            return false;
+        });
+    }
 
     function insertAtCaret(textarea, insertText) {
         textarea = textarea[0];
