17524e5e2cf7b2b23cb363eb74267aed05aee670
Ben Dean-Kawamura <ben@pculture.org>
2016-02-29 12:12:40 -0500

    Overlay banner improvements

--- overlaybanner/admin.py
+++ overlaybanner/admin.py
@@ -22,6 +22,7 @@ from overlaybanner.models import OverlayBanner
 
 @admin.register(OverlayBanner)
 class OverlayBannerAdmin(admin.ModelAdmin):
+    list_display = ['active', 'name']
     fields = ['name', 'html']
     actions = ['activate']
 
